f3d3c34ac13fe4b57d239b74ee8045ac71d803e8
mhDx92@gmail.com <mhdx92@gmail.com>
2019-12-20 15:52:01 +0330

    example added

---
+++ example/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:persian_number_utility/persian_number_utility.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: NumToStr(),
+    );
+  }
+}
+
+class NumToStr extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    String number = "100092";//number here
+
+    return Scaffold(
+        body: new Center(
+            child: new Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.max,
+      children: <Widget>[
+        new Text(number),
+        new Text(NumberUtility.toWord(number, NumStrLanguage.Farsi)),//صد هزار و نود و دو
+        new Text(NumberUtility.toWord(number, NumStrLanguage.English)),//one hundred thousand ninety two
+        new Text(NumberUtility.isNumeric(number).toString()),//true
+        new Text(NumberUtility.seRagham(number)),//100,092
+        new Text(NumberUtility.seRagham(number, separator: "."))//100.092
+      ],
+    )));
+  }
+}
