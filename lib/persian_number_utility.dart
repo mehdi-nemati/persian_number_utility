77e7251d43143073c634cde176d3ceb7e4fe8657
Mohsen Meshkini <mmeshkiny@yahoo.com>
2021-08-01 20:38:13 +0430

    export is missing :D

--- lib/persian_number_utility.dart
+++ lib/persian_number_utility.dart
@@ -3,3 +3,4 @@ library persian_number_utility;
 export 'src/methods.dart';
 export 'src/extensions.dart';
 export 'src/enums.dart';
+export 'src/formatters/perisn_digit_only_text_input_formatter.dart'
